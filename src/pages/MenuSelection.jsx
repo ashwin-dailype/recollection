c2c81a331fadaebee2124691f2933b868adde3e9
gyanchith-dailype <gyanchith@dailype.in>
2024-03-16 12:40:23 +0530

    minor changes

--- src/pages/MenuSelection.jsx
+++ src/pages/MenuSelection.jsx
@@ -3,13 +3,16 @@ import { ChakraProvider, Button, HStack } from "@chakra-ui/react";
 
 function MenuSelection() {
     return (
-        <ChakraProvider>
           <HStack spacing={4}>
           <Button colorScheme='teal' variant='outline'>Pay Loan Installments</Button>
           <Button colorScheme='teal' variant='outline'>Notice Generation</Button>
           </HStack>
-        </ChakraProvider>
       );
 }
 
 export default MenuSelection
+
+// <Box px="3">
+// <SearchUser onSearch={handleSearchChange} />
+// <DisplayTable users={data} search={search} />
+// </Box>
