9490b2fa12df8bd1fa116e61a32b4bacb9ab3e23
gyanchith-dailype <gyanchith@dailype.in>
2024-05-21 21:34:19 +0530

    design changes

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/recollection/"
-});
